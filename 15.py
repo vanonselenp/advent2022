54c06c0da81b3288ce340da9ecd6cadd8da1184c
Peter Van Onselen <peter.vanonselen@cazoo.co.uk>
2022-12-22 22:58:23 +0000

    main: working algo, bad design

--- 15.py
+++ 15.py
@@ -17,6 +17,38 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
 
 example2 = """Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
 
+actual = """Sensor at x=2832148, y=322979: closest beacon is at x=3015667, y=-141020
+Sensor at x=1449180, y=3883502: closest beacon is at x=2656952, y=4188971
+Sensor at x=2808169, y=1194666: closest beacon is at x=3015667, y=-141020
+Sensor at x=1863363, y=2435968: closest beacon is at x=2166084, y=2883057
+Sensor at x=3558230, y=2190936: closest beacon is at x=3244164, y=2592191
+Sensor at x=711491, y=2444705: closest beacon is at x=617239, y=2988377
+Sensor at x=2727148, y=2766272: closest beacon is at x=2166084, y=2883057
+Sensor at x=2857938, y=3988086: closest beacon is at x=2968511, y=4098658
+Sensor at x=1242410, y=2270153: closest beacon is at x=214592, y=2000000
+Sensor at x=3171784, y=2523127: closest beacon is at x=3244164, y=2592191
+Sensor at x=2293378, y=71434: closest beacon is at x=3015667, y=-141020
+Sensor at x=399711, y=73420: closest beacon is at x=1152251, y=-158441
+Sensor at x=3677529, y=415283: closest beacon is at x=3015667, y=-141020
+Sensor at x=207809, y=2348497: closest beacon is at x=214592, y=2000000
+Sensor at x=60607, y=3403420: closest beacon is at x=617239, y=2988377
+Sensor at x=3767729, y=3136725: closest beacon is at x=4171278, y=3348370
+Sensor at x=3899632, y=3998969: closest beacon is at x=4171278, y=3348370
+Sensor at x=394783, y=1541278: closest beacon is at x=214592, y=2000000
+Sensor at x=1193642, y=642631: closest beacon is at x=1152251, y=-158441
+Sensor at x=122867, y=2661904: closest beacon is at x=214592, y=2000000
+Sensor at x=551012, y=3787568: closest beacon is at x=617239, y=2988377
+Sensor at x=3175715, y=2975144: closest beacon is at x=3244164, y=2592191
+Sensor at x=402217, y=2812449: closest beacon is at x=617239, y=2988377
+Sensor at x=879648, y=1177329: closest beacon is at x=214592, y=2000000
+Sensor at x=1317218, y=2978309: closest beacon is at x=617239, y=2988377
+Sensor at x=3965126, y=1743931: closest beacon is at x=3244164, y=2592191
+Sensor at x=2304348, y=3140055: closest beacon is at x=2166084, y=2883057
+Sensor at x=3380135, y=3650709: closest beacon is at x=2968511, y=4098658
+Sensor at x=49224, y=1914296: closest beacon is at x=214592, y=2000000
+Sensor at x=3096228, y=2457233: closest beacon is at x=3244164, y=2592191
+Sensor at x=1415660, y=6715: closest beacon is at x=1152251, y=-158441
+Sensor at x=2616280, y=3548378: closest beacon is at x=2656952, y=4188971"""
 
 def parse(input):
     output = []
@@ -65,21 +97,22 @@ def build_grid(sensors, sizes):
     index = 0
     for group in sensors:
         sensor, beacon = group
+        print(chr(index + 97), sensor)
         grid[sensor[0] - sizes['min']['x']][sensor[1] - sizes['min']['y']] = 'S'
         grid[beacon[0] - sizes['min']['x']][beacon[1] - sizes['min']['y']] = 'B'
-        distance = get_manhattan(sensor, beacon)
 
-        print(distance)
-        for x in range(sensor[0] - distance , sensor[0] + distance + 1):
-            if not (x >= 0 and x < len(grid)):
+        distance = get_manhattan(sensor, beacon)
+        for x in range(sensor[0] - distance, sensor[0] + distance + 1):
+            if not (x >= 0 + sizes['min']['x'] and x < len(grid)):
+                print(x)
                 continue
 
-            for y in range(sensor[1] - distance, sensor[1] + distance + 1):
-                if not (y >= 0 and y < len(grid[0])):
+            for y in range(sensor[1]- distance - 1, sensor[1] + distance + 1):
+                if not (y >= 0 + sizes['min']['y'] and y < len(grid[0])):
                     continue
 
-                if grid[x - sizes['min']['x']][y - sizes['min']['y']] == '.' and get_manhattan(sensor, [x, y]) <= distance:
-                    grid[x - sizes['min']['x']][y - sizes['min']['y']]  = chr(index+97)
+                if grid[x - sizes['min']['x'] ][y - sizes['min']['y']] == '.' and get_manhattan(sensor, [x, y]) <= distance:
+                    grid[x - sizes['min']['x'] ][y - sizes['min']['y'] ] = '#'
 
         index = index + 1
 
@@ -101,9 +134,13 @@ def main(input):
 
     grid = build_grid(sensors, sizes)
 
-    visualise(grid)
+    # visualise(grid)
 
+    # print("".join(grid[10]))
+    line = "".join([x[10 - sizes['min']['y']] for x in grid if x[10 - sizes['min']['y']] == '#'])
+    print(len(line), line)
 
 if __name__ == '__main__':
-    # main(example)
-    main(example2)
+    main(example)
+    main(actual)
+    # main(example2)
