b10dc17ab63d9786b2f08dd2c9e624a4efc7fc3a
Peter Van Onselen <peter.vanonselen@cazoo.co.uk>
2022-12-22 21:39:27 +0000

    main: working

--- 15.py
+++ 15.py
@@ -15,6 +15,8 @@ Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
 
+example2 = """Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
+
 
 def parse(input):
     output = []
@@ -48,7 +50,7 @@ def parse(input):
         if n[1] > max_y:
             max_y = n[1]
 
-    return output, {'min': {'x': min_x, 'y': min_y}, 'max': {'x': max_x, 'y': max_y}}
+    return output, {'min': {'x': min_x - 10, 'y': min_y - 10}, 'max': {'x': max_x + 10, 'y': max_y + 10}}
 
 
 def build_grid(sensors, sizes):
@@ -60,13 +62,32 @@ def build_grid(sensors, sizes):
             row.append('.')
         grid.append(row)
 
+    index = 0
     for group in sensors:
         sensor, beacon = group
-        print(sensor, [sensor[0] - sizes['min']['x'], sensor[1] - sizes['min']['y']])
         grid[sensor[0] - sizes['min']['x']][sensor[1] - sizes['min']['y']] = 'S'
         grid[beacon[0] - sizes['min']['x']][beacon[1] - sizes['min']['y']] = 'B'
+        distance = get_manhattan(sensor, beacon)
+
+        print(distance)
+        for x in range(sensor[0] - distance , sensor[0] + distance + 1):
+            if not (x >= 0 and x < len(grid)):
+                continue
+
+            for y in range(sensor[1] - distance, sensor[1] + distance + 1):
+                if not (y >= 0 and y < len(grid[0])):
+                    continue
+
+                if grid[x - sizes['min']['x']][y - sizes['min']['y']] == '.' and get_manhattan(sensor, [x, y]) <= distance:
+                    grid[x - sizes['min']['x']][y - sizes['min']['y']]  = chr(index+97)
+
+        index = index + 1
 
     return grid
+
+
+def get_manhattan(sensor, becon):
+    return abs(sensor[0] - becon[0]) + abs(sensor[1] - becon[1])
 
 
 def visualise(grid):
@@ -84,4 +105,5 @@ def main(input):
 
 
 if __name__ == '__main__':
-    main(example)
+    # main(example)
+    main(example2)
