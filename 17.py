5420d6d7d5b88e353ddfbc31778d37611f45f501
Peter Van Onselen <peter.vanonselen@cazoo.co.uk>
2023-01-04 10:06:54 +0000

    main: yay

--- 17.py
+++ 17.py
@@ -1,28 +1,30 @@
 example = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"""
 
+empty = "......."
+
 shapes = [
     [
-        "..####."
+        "..@@@@."
     ],
     [
-        "...#...", 
-        "..###..", 
-        "...#..."
+        "...@...", 
+        "..@@@..", 
+        "...@..."
     ],
     [
-        "....#..",
-        "....#..",
-        "..###.."
+        "....@..",
+        "....@..",
+        "..@@@.."
     ],
     [
-        "..#....",
-        "..#....",
-        "..#....",
-        "..#...."
+        "..@....",
+        "..@....",
+        "..@....",
+        "..@...."
     ],
     [
-        "..##...",
-        "..##..."
+        "..@@...",
+        "..@@..."
     ]
 ]
 
@@ -36,19 +38,23 @@ def visualize(grid):
 
 
 def new_rock(grid, current_rock):
-    return shapes[current_rock] + [".......", ".......", "......."] + grid
+    return shapes[current_rock] + [empty, empty, empty] + grid
 
 
 
 def main(input):
-    grid = []
+    grid = ["..####."]
     current_rock = 0
 
     grid = new_rock(grid, current_rock)
     visualize(grid)
 
     for i in range(0, len(grid) - 1):
-        grid[i], grid[i + 1] = grid[i + 1], grid[i]
+        if grid[i + 1] == empty:
+            grid[i], grid[i + 1] = grid[i + 1], grid[i]
+        else: 
+            break
+
         visualize(grid)
 
 if __name__ == '__main__':
